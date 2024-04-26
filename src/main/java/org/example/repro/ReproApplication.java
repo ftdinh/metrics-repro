ed15a76335936c413dbeea9823e1d6099aa63ff8
Francis Dinh <francisdinh@outlook.com>
2024-04-25 17:06:34 -0700

    Configure the MVC Observation filter

--- src/main/java/org/example/repro/ReproApplication.java
+++ src/main/java/org/example/repro/ReproApplication.java
@@ -1,13 +1,29 @@
 package org.example.repro;
 
+import io.micrometer.observation.ObservationRegistry;
+import jakarta.servlet.DispatcherType;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ServerHttpObservationFilter;
 
 @SpringBootApplication
 public class ReproApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReproApplication.class, args);
+    }
+
+    @Bean
+    public FilterRegistrationBean<ServerHttpObservationFilter> webMvcObservationFilter(ObservationRegistry registry) {
+        ServerHttpObservationFilter filter = new ServerHttpObservationFilter(registry);
+        FilterRegistrationBean<ServerHttpObservationFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setOrder(Ordered.HIGHEST_PRECEDENCE + 1);
+        registration.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ASYNC);
+        registration.addUrlPatterns("/metrics");
+        return registration;
     }
 
 }
